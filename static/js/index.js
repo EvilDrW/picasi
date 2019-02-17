523626646596d3160c5f2f0c5595e26f3348fdf5
Keith Wait <keith.w.wait@gmail.com>
2019-02-17 09:12:13 -0600

    refactor into directives

--- static/js/index.js
+++ static/js/index.js
@@ -2,65 +2,79 @@
 
 var app = angular.module('picasi', ['ngMaterial', 'ngMessages', 'leaflet-directive']);
 
-app.controller('TestCtrl', ($scope, $http, $mdDialog, FilterService) => {
-  FilterService.subscribe((filters) => {
-    $http.get('/images', { params: filters })
-    .then((res) => {
-      $scope.imageIDs = res.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  });
+app.directive('imageGrid', ($http, $mdDialog, FilterService) => {
+  return {
+    restrict: 'E',
+    scope: true,
+    templateUrl: 'directives/imageGrid.html',
+    link: (scope, ele, attrs) => {
+      FilterService.subscribe('/images', (data) => {
+        $scope.imageIDs = data;
+      });
 
-  $scope.showImageDetail = (id) => {
-    $mdDialog.show({
-      controller: function($http, $scope, $mdDialog, leafletData) {
-        $http.get(`/images/${id}/metadata`)
-        .then((res) => {
-          $scope.metadata = res.data;
+      scope.showImageDetail = (id) => {
+        $mdDialog.show({
+          controller: function($http, $scope, $mdDialog, leafletData) {
+            $http.get(`/images/${id}/metadata`)
+            .then((res) => {
+              $scope.metadata = res.data;
 
-          $scope.markers = {
-            m1: {
-              lng: res.data.location.coordinates[0],
-              lat: res.data.location.coordinates[1],
-              focus: true
-            }
-          };
+              $scope.markers = {
+                m1: {
+                  lng: res.data.location.coordinates[0],
+                  lat: res.data.location.coordinates[1],
+                  focus: true
+                }
+              };
 
-          $scope.cancel = () => {
-            $mdDialog.cancel();
-          };
+              $scope.cancel = () => {
+                $mdDialog.cancel();
+              };
+            });
+          },
+          templateUrl: 'directives/imagePanel.html'
         });
-      },
-      templateUrl: 'directives/imagePanel.html'
-    });
-  };
-});
-
-app.controller('FilterController', function($scope, FilterService) {
-  $scope.filters = {
-    location: {},
-    date: {},
-    faces: {}
-  };
-
-  $scope.submit = () => {
-    FilterService.clear();
-    if ($scope.filters.date) {
-      FilterService.set('datebegin', $scope.filters.date.begin.getTime());
-      FilterService.set('dateend', $scope.filters.date.end.getTime());
+      };
     }
   };
 });
 
-app.service('FilterService', function() {
+app.directive('filterSetup', function(FilterService) {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/filterSetup.html',
+    scope: true,
+    link: (scope, ele, attrs) => {
+      scope.filters = {
+        location: {},
+        date: {},
+        faces: {}
+      };
+
+      scope.submit = () => {
+        FilterService.clear();
+        if ($scope.filters.date) {
+          FilterService.set('datebegin', $scope.filters.date.begin.getTime());
+          FilterService.set('dateend', $scope.filters.date.end.getTime());
+        }
+      };
+    }
+  };
+});
+
+app.service('FilterService', function($http) {
   var filters = {};
   var subscribers = [];
 
-  this.subscribe = (cb) => {
-    subscribers.push(cb);
-    cb(filters);
+  var notifySubscriber = (sub) => {
+    $http.get(sub.url, { params: filters }).then((res) => {
+      sub.callback(res.data);
+    });
+  };
+
+  this.subscribe = (url, cb) => {
+    subscribers.push({ url: url, callback: cb });
+    notifySubscriber(subscribers[-1]);
   };
 
   this.clear = () => {
@@ -69,9 +83,6 @@ app.service('FilterService', function() {
 
   this.set = (key, val) => {
     filters[key] = val;
-
-    subscribers.forEach((cb) => {
-      cb(filters);
-    });
+    subscribers.forEach(notifySubscriber);
   };
 });
